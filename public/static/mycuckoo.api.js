7112389aad9b0a11b8ae29e676cd6c6a47b2610e
rutine <rutine@163.com>
2017-08-06 18:19:30 +0800

    登录完善

---
+++ public/static/mycuckoo.api.js
@@ -0,0 +1,9 @@
+const host = 'http://localhost:8080';
+const mycuckoo = {
+  
+  login: {
+    firstStepUrl: host + '/login/step/first',
+    secondStepUrl: host + '/login/step/second',
+    thirdStepUrl: host + '/login/step/third',
+  }
+}
